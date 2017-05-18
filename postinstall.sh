b7411c695dd8c00e1a70d1497c36d6fa80474b1f
eXtremeSHOK <extremeshok@users.noreply.github.com>
2017-05-18 14:49:23 +0200

    Update postinstall.sh
    Bugfix frozen/hung update caused by broken ceph systemd script

--- postinstall.sh
+++ postinstall.sh
@@ -41,6 +41,12 @@ sed -i "s/main contrib/main non-free contrib/g" /etc/apt/sources.list
 # Install the latest ceph (jewel, aka ceph 10.2.5 or higher)
 sed -i "s/hammer/jewel/g" /etc/apt/sources.list.d/ceph.list
 
+#Bugfix frozen/hung update caused by broken ceph systemd script
+if [ -f /etc/systemd/system/ceph.service ]; then
+	sed -i "s/=ceph.target/=multi-user.target/" /etc/systemd/system/ceph.service
+	systemctl daemon-reload; systemctl disable ceph.service; systemctl enable ceph.service; systemctl daemon-reexec
+fi
+
 ## Update proxmox and install various system utils
 apt-get update && apt-get -y dist-upgrade --force-yes &&  pveam update
 
