ef1841808665a743e0c2901e60226c5de2f7d1a7
admin@extremeshok.com <admin@extremeshok.com>
2017-08-26 03:56:05 +0200

    Multi-drive support, automatically detect and set raid level (mirror, raidz1, raidz2, raidz3)

--- lvm2zfs.sh
+++ lvm2zfs.sh
@@ -61,7 +61,7 @@ else
 fi
 
 #pve/data
-mylv=$(lvdisplay "$mydev" | sed -n -e 's/^.*\/dev\///p')
+mylv=$(lvdisplay "$mydev" 2> /dev/null | sed -n -e 's/^.*\/dev\///p')
 ret=$?
 if [ $ret == 0 ] ; then
 	echo "Found lv, continuing"
@@ -76,35 +76,44 @@ IFS=' ' read -r -a mddevarray <<< "$(grep "$myraid :" /proc/mdstat | cut -d ' ' 
 #mddevarray="$(grep "$myraid :" /proc/mdstat | cut -d ' ' -f5- | xargs)"
 #mddevarray=(${mddevarray//:/ })
 
-echo "${mddevarray[0]}"
+
 if [ "${mddevarray[0]}" == "" ] ; then
 	echo "ERROR: no devices found for $myraid in /proc/mdstat"
 	exit 0
 fi
 
-#Only need to check there is a minimum of 2 drives detected
-if [ "$mydev" != "" ] && [ "$myraid" != "" ] && [ "$mylv" != "" ] && [ "${mddevarray[0]}" != "" ] && [ "${mddevarray[1]}" != "" ] ; then
+#check there is a minimum of 2 drives detected
+if [ "${#mddevarray[@]}" -lt "2" ] ; then
+  echo "ERROR: less than 2 devices were detected"
+  exit 0
+fi
+
+if [ "$mydev" != "" ] && [ "$myraid" != "" ] && [ "$mylv" != "" ] ; then
 	echo "All required varibles detected"
 else
 	echo "ERROR: required varible not found or the server is already converted to zfs"
 	exit 0
 fi
 
+
+# remove [*] and /dev/ to each record
+echo "Creating the device array"
+for index in "${!mddevarray[@]}" ; do
+    tempmddevarraystring="${mddevarray[index]}"
+    mddevarray[$index]="/dev/${tempmddevarraystring%\[*\]}"
+done
+
 echo "Destroying LV (logical volume)"
 umount -l "$mypart"
 lvremove "/dev/$mylv" -y
+
 echo "Destroying MD (linux raid)"
 mdadm --stop "/dev/$myraid"
 mdadm --remove "/dev/$myraid"
 
 for mydev in "${mddevarray[@]}" ; do
     echo "zeroing $mydev"
-    mdadm --zero-superblock "/dev/$mydev"
-done
-
-# add the /dev to each record
-for index in "${!mddevarray[@]}" ; do
-    mddevarray[$index]="/dev/${mddevarray[index]}"
+    mdadm --zero-superblock "$mydev"
 done
 
 # #used to make a max free space lvm
@@ -112,15 +121,25 @@ done
 if [ "${#mddevarray[@]}" -eq "2" ] ; then
   echo "Creating ZFS mirror (raid1)"
   zpool create -f -o ashift=12 -O compression=lz4 rpool mirror "${mddevarray[@]}"
-elif [ "${#mddevarray[@]}" -ge "3" ] && [ "${#mddevarray[@]}" -lt "5" ] ; then
+  ret=$?
+elif [ "${#mddevarray[@]}" -ge "3" ] && [ "${#mddevarray[@]}" -le "5" ] ; then
   echo "Creating ZFS raidz-1 (raid5)"
   zpool create -f -o ashift=12 -O compression=lz4 rpool raidz "${mddevarray[@]}"
+  ret=$?
 elif [ "${#mddevarray[@]}" -ge "6" ] && [ "${#mddevarray[@]}" -lt "11" ] ; then
   echo "Creating ZFS raidz-2 (raid6)"
   zpool create -f -o ashift=12 -O compression=lz4 rpool raidz2 "${mddevarray[@]}"
+  ret=$?
 elif [ "${#mddevarray[@]}" -ge "11" ] ; then
   echo "Creating ZFS raidz-3 (raid7)"
   zpool create -f -o ashift=12 -O compression=lz4 rpool raidz3 "${mddevarray[@]}"
+  ret=$?
+fi
+
+
+if [ $ret != 0 ] ; then
+	echo "ERROR: creating ZFS"
+	exit 0
 fi
 
 echo "Setting Additional Options"
