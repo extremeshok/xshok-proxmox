6791e9a4558f365611b7dd7943b0ea548b75514f
admin@extremeshok.com <admin@extremeshok.com>
2017-08-26 03:30:08 +0200

    added support for zraid1, zraid2, zraid3. Support an unlimited number of devices

--- lvm2zfs.sh
+++ lvm2zfs.sh
@@ -71,15 +71,19 @@ else
 	exit 0
 fi
 
-IFS=' ' read -r -a array <<< "$(grep "$myraid :" /proc/mdstat | cut -d ' ' -f5- | xargs)"
-echo "${array[0]}"
-if [ "${array[0]}" == "" ] ; then
+IFS=' ' read -r -a mddevarray <<< "$(grep "$myraid :" /proc/mdstat | cut -d ' ' -f5- | xargs)"
+#without IFS
+#mddevarray="$(grep "$myraid :" /proc/mdstat | cut -d ' ' -f5- | xargs)"
+#mddevarray=(${mddevarray//:/ })
+
+echo "${mddevarray[0]}"
+if [ "${mddevarray[0]}" == "" ] ; then
 	echo "ERROR: no devices found for $myraid in /proc/mdstat"
 	exit 0
 fi
 
-
-if [ "$mydev" != "" ] && [ "$myraid" != "" ] && [ "$mylv" != "" ] && [ "$mydev1" != "" ] && [ "$mydev2" != "" ] ; then
+#Only need to check there is a minimum of 2 drives detected
+if [ "$mydev" != "" ] && [ "$myraid" != "" ] && [ "$mylv" != "" ] && [ "${mddevarray[0]}" != "" ] && [ "${mddevarray[1]}" != "" ] ; then
 	echo "All required varibles detected"
 else
 	echo "ERROR: required varible not found or the server is already converted to zfs"
@@ -92,14 +96,32 @@ lvremove "/dev/$mylv" -y
 echo "Destroying MD (linux raid)"
 mdadm --stop "/dev/$myraid"
 mdadm --remove "/dev/$myraid"
-mdadm --zero-superblock "/dev/$mydev1"
-mdadm --zero-superblock "/dev/$mydev2"
+
+for mydev in "${mddevarray[@]}" ; do
+    echo "zeroing $mydev"
+    mdadm --zero-superblock "/dev/$mydev"
+done
+
+# add the /dev to each record
+for index in "${!mddevarray[@]}" ; do
+    mddevarray[$index]="/dev/${mddevarray[index]}"
+done
 
 # #used to make a max free space lvm
 # #lvcreate -n ZFS pve -l 100%FREE -y
-
-echo "Creating ZFS"
-zpool create -f -o ashift=12 -O compression=lz4 rpool mirror "/dev/$mydev1" "/dev/$mydev2"
+if [ "${#mddevarray[@]}" -eq "2" ] ; then
+  echo "Creating ZFS mirror (raid1)"
+  zpool create -f -o ashift=12 -O compression=lz4 rpool mirror "${mddevarray[@]}"
+elif [ "${#mddevarray[@]}" -ge "3" ] && [ "${#mddevarray[@]}" -lt "5" ] ; then
+  echo "Creating ZFS raidz-1 (raid5)"
+  zpool create -f -o ashift=12 -O compression=lz4 rpool raidz "${mddevarray[@]}"
+elif [ "${#mddevarray[@]}" -ge "6" ] && [ "${#mddevarray[@]}" -lt "11" ] ; then
+  echo "Creating ZFS raidz-2 (raid6)"
+  zpool create -f -o ashift=12 -O compression=lz4 rpool raidz2 "${mddevarray[@]}"
+elif [ "${#mddevarray[@]}" -ge "11" ] ; then
+  echo "Creating ZFS raidz-3 (raid7)"
+  zpool create -f -o ashift=12 -O compression=lz4 rpool raidz3 "${mddevarray[@]}"
+fi
 
 echo "Setting Additional Options"
 zfs set compression=on rpool
@@ -112,7 +134,6 @@ zfs set dedup=off rpool
 echo "Creating Secondary ZFS Pools"
 zfs create rpool/vm-disks
 zfs create -o mountpoint=/backup rpool/backup
-
 zpool export rpool
 
 echo "Cleaning up fstab / mounts"
