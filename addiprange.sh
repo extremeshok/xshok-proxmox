3f0133a0f0d6f31091a7742df872e3bfa660628d
admin@extremeshok.com <admin@extremeshok.com>
2017-08-30 02:14:58 +0200

    cleanups

--- addiprange.sh
+++ addiprange.sh
@@ -66,10 +66,8 @@ fi
 
 ## Not used because this is slower than the if/else block
 #cdr2mask () {
-#   # Number of args to shift, 255..255, first non-255 byte, zeroes
 #   set -- $(( 5 - ($1 / 8) )) 255 255 255 255 $(( (255 << (8 - ($1 % 8))) & 255 )) 0 0 0
-#	 if [[ "$1" -gt 1 ]] ; then shift "$1" ; else shift ; fi
-#   echo "${1-0}.${2-0}.${3-0}.${4-0}"
+#	  if [[ "$1" -gt 1 ]] ; then shift "$1" ; else shift ; fi ;  echo "${1-0}.${2-0}.${3-0}.${4-0}"
 #}
 #netmask="$(cdr2mask ""$cidr")"
 # get the netmask
