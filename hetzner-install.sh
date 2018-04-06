dc2792024d81add5cda2007dfd2ec9c3b01a09ca
eXtremeSHOK <extremeshok@users.noreply.github.com>
2018-04-06 18:08:21 +0200

    Update hetzner-install.sh

--- hetzner-install.sh
+++ hetzner-install.sh
@@ -34,6 +34,7 @@ USE_LVM="TRUE"
 
 ##############################################################
 
+installimage_bin="/root/.oldroot/nfs/install/installimage"
 
 MY_HOSTNAME="$1"
 if [ "$MY_HOSTNAME" == "" ]; then
@@ -75,9 +76,9 @@ echo "wget https://raw.githubusercontent.com/extremeshok/xshok-proxmox/master/po
 if grep -q '#!/bin/bash' "/post-install"; then
   echo "Starting Installer"
   if [ "$USE_LVM" == "TRUE" ]; then
-    installimage -a -i "root/images/Debian-94-stretch-64-minimal.tar.gz" -g -s en -x /post-install -n "${MY_HOSTNAME}" -b grub -d sda,sdb -r yes -l 1 -p "/:ext4:40G,swap:swap:${MY_SWAP}G,lvm:vg0:all" -v "vg0:data:/var/lib/vz:ext4:all"
+    $installimage_bin -a -i "root/images/Debian-94-stretch-64-minimal.tar.gz" -g -s en -x /post-install -n "${MY_HOSTNAME}" -b grub -d sda,sdb -r yes -l 1 -p "/:ext4:40G,swap:swap:${MY_SWAP}G,lvm:vg0:all" -v "vg0:data:/var/lib/vz:ext4:all"
   else
-    installimage -a -i "root/images/Debian-94-stretch-64-minimal.tar.gz" -g -s en -x /post-install -n "${MY_HOSTNAME}" -b grub -d sda,sdb -r yes -l 1 -p "/:ext4:all,swap:swap:${MY_SWAP}G"
+    $installimage_bin -a -i "root/images/Debian-94-stretch-64-minimal.tar.gz" -g -s en -x /post-install -n "${MY_HOSTNAME}" -b grub -d sda,sdb -r yes -l 1 -p "/:ext4:all,swap:swap:${MY_SWAP}G"
   fi
   
 else
