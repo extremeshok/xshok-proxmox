2d2a09007d438e6c2e9d2961bfa43d60cdfeef6f
eXtremeSHOK <extremeshok@users.noreply.github.com>
2018-04-06 17:17:15 +0200

    Update hetzner-install.sh

--- hetzner-install.sh
+++ hetzner-install.sh
@@ -75,7 +75,7 @@ echo "wget https://raw.githubusercontent.com/extremeshok/xshok-proxmox/master/po
 if grep -q '#!/bin/bash' "/root/post-install-proxmox5"; then
   echo "Starting Installer"
   if [ "$USE_LVM" == "TRUE" ]; then
-    installimage -a -i "root/images/Debian-94-stretch-64-minimal.tar.gz" -g -s en -x /post-install -n "${MY_HOSTNAME}" -b grub -d sda,sdb -r yes -l 1 -p "/boot:ext4:1024M,lvm:vg0:all" -v "vg0:root:/:ext4:50G,vg0:swap:swap:swap:${MY_SWAP}G"
+    installimage -a -i "root/images/Debian-94-stretch-64-minimal.tar.gz" -g -s en -x /post-install -n "${MY_HOSTNAME}" -b grub -d sda,sdb -r yes -l 1 -p "/:ext4:40G,swap:swap:${MY_SWAP}G,lvm:vg0:all" -v "vg0:data:/var/lib/vz:ext4:all"
   else
     installimage -a -i "root/images/Debian-94-stretch-64-minimal.tar.gz" -g -s en -x /post-install -n "${MY_HOSTNAME}" -b grub -d sda,sdb -r yes -l 1 -p "/:ext4:all,swap:swap:${MY_SWAP}G"
   fi
