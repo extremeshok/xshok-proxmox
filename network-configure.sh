97c35795fa36019f90123d50ca65161ae05bc20a
eXtremeSHOK <extremeshok@users.noreply.github.com>
2018-04-10 16:35:42 +0200

    Update network-configure.sh
    Added Public Network (vmbr0) to isc-dhcp-server

--- network-configure.sh
+++ network-configure.sh
@@ -61,6 +61,123 @@ net.ipv6.conf.all.forwarding=1
 EOF
 fi
 
+# Auto detect the existing network settings... this is all for ipv4
+echo "Auto detecting existing network settings"
+default_interface="$(ip route | awk '/default/ { print $5 }' | grep -v "vmbr")"
+if [ "$default_interface" == "" ]; then
+  #filter the interfaces to get the default interface and which is not down and not a virtual bridge
+  default_interface="$(ip link | sed -e '/state DOWN / { N; d; }' | sed -e '/veth[0-9].*:/ { N; d; }' | sed -e '/vmbr[0-9].*:/ { N; d; }' | sed -e '/tap[0-9].*:/ { N; d; }' | sed -e '/lo:/ { N; d; }' | head -n 1 | cut -d':' -f 2 | xargs)"
+fi
+if [ "$default_interface" == "" ]; then
+  echo "ERROR: Could not detect default interface"
+  exit 1
+fi
+default_v4gateway="$(ip route | awk '/default/ { print $3 }')"
+default_v4="$(ip -4 addr show dev "$default_interface" | awk '/inet/ { print $2 }' )"
+default_v4ip=${default_v4%/*}
+default_v4mask=${default_v4#*/}
+if [ "$default_v4mask" == "$default_v4ip" ] ;then
+  default_v4netmask="$(ifconfig vmbr0 | awk '/netmask/ { print $4 }')"
+else
+  if [ "$default_v4mask" -lt "1" ] || [ "$default_v4mask" -gt "32" ] ; then
+    echo "ERROR: Invalid CIDR $default_v4mask"
+    exit 1
+  fi
+  cdr2mask () {
+    set -- $(( 5 - ($1 / 8) )) 255 255 255 255 $(( (255 << (8 - ($1 % 8))) & 255 )) 0 0 0
+    if [[ "$1" -gt 1 ]] ; then shift "$1" ; else shift ; fi ;  echo "${1-0}.${2-0}.${3-0}.${4-0}"
+  }
+  default_v4netmask="$(cdr2mask "$default_v4mask")"
+fi
+
+if [ "$default_v4ip" == "" ] || [ "$default_v4netmask" == "" ] || [ "$default_v4gateway" == "" ]; then
+  echo "ERROR: Could not detect all IPv4 varibles"
+  echo "IP: ${default_v4ip} Netmask: ${default_v4netmask} Gateway: ${default_v4gateway}"
+  exit 1
+fi
+
+# Generate an array containing parts of the default_v4ip
+IFS='.' read -r -a default_v4ip_array <<< "$default_v4ip"
+
+cp "$network_interfaces_file" "${network_interfaces_file}.$(date +"%Y-%m-%d_%H-%M-%S")"
+
+cat > "$network_interfaces_file" <<EOF
+###### eXtremeSHOK.com
+
+# Load extra files, ie for extra gateways
+source /etc/network/interfaces.d/*
+
+auto lo
+iface lo inet loopback
+iface lo inet6 loopback
+
+### IPv4 ###
+# Main IPv4 from Host
+auto ${default_interface}
+iface ${default_interface} inet static
+  address ${default_v4ip}
+  netmask ${default_v4netmask}
+  gateway ${default_v4gateway}
+  pointopoint ${default_v4gateway}
+
+# VM-Bridge used by Proxmox Guests
+auto vmbr0
+iface vmbr0 inet static
+  address cat /
+  netmask ${default_v4netmask}
+  bridge_ports none
+  bridge_stp off
+  bridge_fd 0
+  bridge_maxwait 0
+
+EOF
+
+default_v6="$(ip -6 addr show dev "$default_interface" | awk '/global/ { print $2}')"
+default_v6ip=${default_v6%/*}
+default_v6mask=${default_v6#*/}
+default_v6gateway="$(ip -6 route | awk '/default/ { print $3 }')"
+
+if [ "$default_v6ip" != "" ] && [ "$default_v6mask" != "" ] && [ "$default_v6gateway" != "" ]; then
+cat >> "$network_interfaces_file"  << EOF
+### IPv6 ###
+iface ${default_interface} inet6 static
+  address ${default_v6ip}
+  netmask ${default_v6mask}
+  gateway ${default_v6gateway}
+
+iface vmbr0 inet6 static
+  address ${default_v6ip}
+  netmask 64
+
+EOF
+fi
+
+cat >> "$network_interfaces_file"  << EOF
+### Private NAT network
+auto vmbr1
+iface vmbr1 inet static
+  address  10.10.10.1
+  netmask  255.255.255.0
+  bridge_ports none
+  bridge_stp off
+  bridge_fd 0
+  post-up   iptables -t nat -A POSTROUTING -s '10.10.10.0/24' -o ${default_interface} -j MASQUERADE
+  post-down iptables -t nat -D POSTROUTING -s '10.10.10.0/24' -o ${default_interface} -j MASQUERADE
+
+EOF
+
+cat >> "$network_interfaces_file"  << EOF
+### Extra IP/IP Ranges ###
+# Use ./network-addiprange.sh script to add ip/ip ranges or edit the examples below
+#
+## Example add IP range 176.9.216.192/27
+# up route add -net 94.130.239.192 netmask 255.255.255.192 gw ${default_v4gateway} dev ${default_interface}
+## Example add IP 176.9.123.158
+# up route add -net 176.9.123.158 netmask 255.255.255.255 gw ${default_v4gateway} dev ${default_interface}
+
+EOF
+
+
 # Configure isc-dhcp-server
 if [ -f "/etc/default/isc-dhcp-server" ] ; then
   cp /etc/default/isc-dhcp-server "/etc/default/isc-dhcp-server.$(date +"%Y-%m-%d_%H-%M-%S")"
@@ -126,142 +243,26 @@ subnet 10.10.10.0 netmask 255.255.255.0 {
   option ms-classless-static-routes 32, 10, 10, 10, 1, 0, 0, 0, 0, 0, 10, 10, 10, 1;
 }
 
-### Extra IP Ranges ###
-## Example IP 11.22.33.44 for host my.example.com via 5.6.7.8
-#subnet 0.0.0.0 netmask 0.0.0.0 {
-#  authoritative;
-#  default-lease-time 21600000;
-#  max-lease-time 432000000;
-#
-#  option routers 5.6.7.8;
-#  option subnet-mask 255.255.255.0;
-#  #option broadcast-address 5.6.7.8;
-#  option domain-name-servers 1.1.1.1,8.8.8.8,8.8.4.4;
-#
-#  option rfc3442-classless-static-routes 24, 5, 6, 7, 8, 0, 0, 0, 0, 0, 5, 6, 7, 8;
-#  option ms-classless-static-routes 24, 5, 6, 7, 8, 0, 0, 0, 0, 0, 5, 6, 7, 8;
-#
+### vmbr0 ; Public Network
+subnet 0.0.0.0 netmask 0.0.0.0 {
+  authoritative;
+  default-lease-time 21600000;
+  max-lease-time 432000000;
+  option routers ${default_v4ip};  
+  option rfc3442-classless-static-routes 24, ${default_v4ip_array[0]}, ${default_v4ip_array[1]}, ${default_v4ip_array[2]}, ${default_v4ip_array[3]}, 0, 0, 0, 0, 0, ${default_v4ip_array[0]}, ${default_v4ip_array[1]}, ${default_v4ip_array[2]}, ${default_v4ip_array[3]};
+  option ms-classless-static-routes 24, ${default_v4ip_array[0]}, ${default_v4ip_array[1]}, ${default_v4ip_array[2]}, ${default_v4ip_array[3]}, 0, 0, 0, 0, 0, ${default_v4ip_array[0]}, ${default_v4ip_array[1]}, ${default_v4ip_array[2]}, ${default_v4ip_array[3]};
+}
+
+## Assign a specific IP to a VM/CT with MAC 9E:94:13:7D:F3:0E to the IP 11.22.33.44 for host my.example.com
+## set the bridge to vmbr0 and the MAC address will need to match the "hardware ethernet" MAC
 #  host my.example.com {
 #    hardware ethernet 9E:94:13:7D:F3:0E;
 #    fixed-address 11.22.33.44;
 #  }
-#}
 
 EOF
+
 systemctl enable isc-dhcp-server
-
-# Auto detect the existing network settings... this is all for ipv4
-echo "Auto detecting existing network settings"
-default_interface="$(ip route | awk '/default/ { print $5 }' | grep -v "vmbr")"
-if [ "$default_interface" == "" ]; then
-  #filter the interfaces to get the default interface and which is not down and not a virtual bridge
-  default_interface="$(ip link | sed -e '/state DOWN / { N; d; }' | sed -e '/veth[0-9].*:/ { N; d; }' | sed -e '/vmbr[0-9].*:/ { N; d; }' | sed -e '/tap[0-9].*:/ { N; d; }' | sed -e '/lo:/ { N; d; }' | head -n 1 | cut -d':' -f 2 | xargs)"
-fi
-if [ "$default_interface" == "" ]; then
-  echo "ERROR: Could not detect default interface"
-  exit 1
-fi
-default_v4gateway="$(ip route | awk '/default/ { print $3 }')"
-default_v4="$(ip -4 addr show dev "$default_interface" | awk '/inet/ { print $2 }' )"
-default_v4ip=${default_v4%/*}
-default_v4mask=${default_v4#*/}
-if [ "$default_v4mask" == "$default_v4ip" ] ;then
-  default_v4netmask="$(ifconfig vmbr0 | awk '/netmask/ { print $4 }')"
-else
-  if [ "$default_v4mask" -lt "1" ] || [ "$default_v4mask" -gt "32" ] ; then
-    echo "ERROR: Invalid CIDR $default_v4mask"
-    exit 1
-  fi
-  cdr2mask () {
-    set -- $(( 5 - ($1 / 8) )) 255 255 255 255 $(( (255 << (8 - ($1 % 8))) & 255 )) 0 0 0
-    if [[ "$1" -gt 1 ]] ; then shift "$1" ; else shift ; fi ;  echo "${1-0}.${2-0}.${3-0}.${4-0}"
-  }
-  default_v4netmask="$(cdr2mask "$default_v4mask")"
-fi
-
-if [ "$default_v4ip" == "" ] || [ "$default_v4netmask" == "" ] || [ "$default_v4gateway" == "" ]; then
-  echo "ERROR: Could not detect all IPv4 varibles"
-  echo "IP: ${default_v4ip} Netmask: ${default_v4netmask} Gateway: ${default_v4gateway}"
-  exit 1
-fi
-
-cp "$network_interfaces_file" "${network_interfaces_file}.$(date +"%Y-%m-%d_%H-%M-%S")"
-
-cat > "$network_interfaces_file" <<EOF
-###### eXtremeSHOK.com
-
-# Load extra files, ie for extra gateways
-source /etc/network/interfaces.d/*
-
-auto lo
-iface lo inet loopback
-iface lo inet6 loopback
-
-### IPv4 ###
-# Main IPv4 from Host
-auto ${default_interface}
-iface ${default_interface} inet static
-  address ${default_v4ip}
-  netmask ${default_v4netmask}
-  gateway ${default_v4gateway}
-  pointopoint ${default_v4gateway}
-
-# VM-Bridge used by Proxmox Guests
-auto vmbr0
-iface vmbr0 inet static
-  address ${default_v4ip}
-  netmask ${default_v4netmask}
-  bridge_ports none
-  bridge_stp off
-  bridge_fd 0
-  bridge_maxwait 0
-
-EOF
-
-default_v6="$(ip -6 addr show dev "$default_interface" | awk '/global/ { print $2}')"
-default_v6ip=${default_v6%/*}
-default_v6mask=${default_v6#*/}
-default_v6gateway="$(ip -6 route | awk '/default/ { print $3 }')"
-
-if [ "$default_v6ip" != "" ] && [ "$default_v6mask" != "" ] && [ "$default_v6gateway" != "" ]; then
-cat >> "$network_interfaces_file"  << EOF
-### IPv6 ###
-iface ${default_interface} inet6 static
-  address ${default_v6ip}
-  netmask ${default_v6mask}
-  gateway ${default_v6gateway}
-
-iface vmbr0 inet6 static
-  address ${default_v6ip}
-  netmask 64
-
-EOF
-fi
-
-cat >> "$network_interfaces_file"  << EOF
-### Private NAT network
-auto vmbr1
-iface vmbr1 inet static
-  address  10.10.10.1
-  netmask  255.255.255.0
-  bridge_ports none
-  bridge_stp off
-  bridge_fd 0
-  post-up   iptables -t nat -A POSTROUTING -s '10.10.10.0/24' -o ${default_interface} -j MASQUERADE
-  post-down iptables -t nat -D POSTROUTING -s '10.10.10.0/24' -o ${default_interface} -j MASQUERADE
-
-EOF
-
-cat >> "$network_interfaces_file"  << EOF
-### Extra IP/IP Ranges ###
-# Use ./network-addiprange.sh script to add ip/ip ranges or edit the examples below
-#
-## Example add IP range 176.9.216.192/27
-# up route add -net 94.130.239.192 netmask 255.255.255.192 gw ${default_v4gateway} dev ${default_interface}
-## Example add IP 176.9.123.158
-# up route add -net 176.9.123.158 netmask 255.255.255.255 gw ${default_v4gateway} dev ${default_interface}
-
-EOF
 
 ## Script Finish
 echo -e '\033[1;33m Finished....please restart the system \033[0m'
