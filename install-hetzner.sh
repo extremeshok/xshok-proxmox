507ef687d50c48cf9d69dfd7df860e94649ed804
eXtremeSHOK <admin@extremeshok.com>
2018-09-01 13:30:50 +0200

    # Detect discs for software raid and ensure sda and sd? are the identical size # autoselects the second drive to raid with sda # sda is always used, as sda is generally the primary boot disk # disables raid if a suitable second disk is not found

--- install-hetzner.sh
+++ install-hetzner.sh
@@ -38,13 +38,37 @@ installimage_bin="/root/.oldroot/nfs/install/installimage"
 MY_HOSTNAME="$1"
 if [ "$MY_HOSTNAME" == "" ]; then
   echo "Please set a hostname"
-  echo "$0 host.name" 
+  echo "$0 host.name"
   exit 1
 fi
 
+# Detect discs for software raid and ensure sda and sd? are the identical size
+# autoselects the second drive to raid with sda
+# sda is always used, as sda is generally the primary boot disk
+# disables raid if a suitable second disk is not found
+if [[ $(awk '/sda$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) -eq $(awk '/sdb$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) ]]; then
+  MY_RAID_SLAVE=",sdb"
+  MY_RAID_ENABLE="yes"
+elif [[ $(awk '/sda$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) -eq $(awk '/sdc$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) ]]; then
+  MY_RAID_SLAVE=",sdc"
+  MY_RAID_ENABLE="yes"
+elif [[ $(awk '/sda$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) -eq $(awk '/sdd$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) ]]; then
+  MY_RAID_SLAVE=",sdd"
+  MY_RAID_ENABLE="yes"
+elif [[ $(awk '/sda$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) -eq $(awk '/sde$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) ]]; then
+  MY_RAID_SLAVE=",sde"
+  MY_RAID_ENABLE="yes"
+elif [[ $(awk '/sda$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) -eq $(awk '/sdf$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) ]]; then
+  MY_RAID_SLAVE=",sdf"
+  MY_RAID_ENABLE="yes"
+else
+  MY_RAID_SLAVE=""
+  MY_RAID_ENABLE="no"
+fi
 # HDD less than 130gb = 16GB swap
 # RAM less or equal to 64GB = 32GB swap
 # RAM more than 64GB = 64GB swap
+
 
 if [ "$MY_SWAP" == "" ]; then
   echo "Detecting and setting optimal swap partition size"
@@ -58,10 +82,10 @@ if [ "$MY_SWAP" == "" ]; then
     MY_SWAP="16"
   fi
 else
-  if ! [ $MY_SWAP -ne 0 -o $MY_SWAP -eq 0 2>/dev/null ] ; then
-    echo "MY_SWAP is not an integer"
+  if ! [[ $MY_SWAP =~ ^[0-9]+$ ]] ; then
+    echo "error: MY_SWAP is Not a number"
     exit 1
-  fi  
+  fi
 fi
 
 echo "Set swap size to ${MY_SWAP} GBytes"
@@ -75,12 +99,13 @@ echo "wget https://raw.githubusercontent.com/extremeshok/xshok-proxmox/master/in
 if grep -q '#!/bin/bash' "/post-install"; then
   chmod 777 /post-install
   echo "Starting Installer"
+
   if [ "$USE_LVM" == "TRUE" ]; then
-    $installimage_bin -a -i "root/images/Debian-94-stretch-64-minimal.tar.gz" -g -s en -x /post-install -n "${MY_HOSTNAME}" -b grub -d sda,sdb -r yes -l 1 -p "/:ext4:40G,swap:swap:${MY_SWAP}G,lvm:vg0:all" -v "vg0:data:/var/lib/vz:ext4:all"
+    $installimage_bin -a -i "root/images/Debian-94-stretch-64-minimal.tar.gz" -g -s en -x /post-install -n "${MY_HOSTNAME}" -b grub -d "sda${MY_RAID_SLAVE}" -r "${MY_RAID_ENABLE}" -l 1 -p "/:ext4:40G,swap:swap:${MY_SWAP}G,lvm:vg0:all" -v "vg0:data:/var/lib/vz:ext4:all"
   else
-    $installimage_bin -a -i "root/images/Debian-94-stretch-64-minimal.tar.gz" -g -s en -x /post-install -n "${MY_HOSTNAME}" -b grub -d sda,sdb -r yes -l 1 -p "/:ext4:all,swap:swap:${MY_SWAP}G"
+    $installimage_bin -a -i "root/images/Debian-94-stretch-64-minimal.tar.gz" -g -s en -x /post-install -n "${MY_HOSTNAME}" -b grub -d "sda${MY_RAID_SLAVE}" -r "${MY_RAID_ENABLE}" -l 1 -p "/:ext4:all,swap:swap:${MY_SWAP}G"
   fi
-  
+
 else
   echo "Failed to fetch post-install"
   exit 1
