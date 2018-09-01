b89dd017e82244f1e2d0e1e22bfa0a754457fd87
eXtremeSHOK <admin@extremeshok.com>
2018-09-01 23:44:11 +0200

    better raid 1 logic, match drive of the same size to sba (eg. sdc) detect and enable raid 10 on 4 idential drives (sda,sdb,sdc,sdd) automatically set swap partition size create zfs-cache and zfs-slog partitions in ssd is detected

--- install-hetzner.sh
+++ install-hetzner.sh
@@ -30,8 +30,10 @@
 #
 ################################################################################
 
-#set size of swap file or leave blank for autoconfig, USE NUMBER ONLY, will be in gbytes
+#set size of swap partition or leave blank for autoconfig, USE NUMBER ONLY, will be in gbytes
 MY_SWAP=""
+#set size of cache partition or leave blank for autoconfig, USE NUMBER ONLY, will be in gbytes
+MY_CACHE=""
 #comment out to disable LVM and use a very simple partition setup of / and swap
 USE_LVM="TRUE"
 
@@ -46,45 +48,47 @@ if [ "$MY_HOSTNAME" == "" ]; then
   exit 1
 fi
 
+##### CONFIGURE RAID
 # Detect discs for software raid and ensure sda and sd? are the identical size
 # autoselects the second drive to raid with sda
 # sda is always used, as sda is generally the primary boot disk
 # disables raid if a suitable second disk is not found
 if [[ $(awk '/sda$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) -eq $(awk '/sdb$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) ]]; then
+  MY_RAID_ENABLE="yes"
   MY_RAID_SLAVE=",sdb"
-  MY_RAID_ENABLE="yes"
 elif [[ $(awk '/sda$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) -eq $(awk '/sdc$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) ]]; then
+  MY_RAID_ENABLE="yes"
   MY_RAID_SLAVE=",sdc"
-  MY_RAID_ENABLE="yes"
 elif [[ $(awk '/sda$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) -eq $(awk '/sdd$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) ]]; then
+  MY_RAID_ENABLE="yes"
   MY_RAID_SLAVE=",sdd"
-  MY_RAID_ENABLE="yes"
 elif [[ $(awk '/sda$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) -eq $(awk '/sde$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) ]]; then
+  MY_RAID_ENABLE="yes"
   MY_RAID_SLAVE=",sde"
-  MY_RAID_ENABLE="yes"
 elif [[ $(awk '/sda$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) -eq $(awk '/sdf$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) ]]; then
-  MY_RAID_SLAVE=",sdf"
   MY_RAID_ENABLE="yes"
+  MY_RAID_SLAVE=",sdf"
 else
-  MY_RAID_SLAVE=""
   MY_RAID_ENABLE="no"
+  MY_RAID_SLAVE=""
 fi
-
 #test for possible raid10, using 4 devices of equal size
 if [ "$MY_RAID_ENABLE" == "yes" ]; then
   if [[ $(awk '/sda$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) -eq $(awk '/sdb$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) ]] && [[ $(awk '/sda$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) -eq $(awk '/sdc$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) ]] && [[ $(awk '/sda$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) -eq $(awk '/sdd$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) ]] ; then
     MY_RAID_LEVEL="10"
+    MY_RAID_SLAVE=",sdb,sdc,sdd"
   else
     MY_RAID_LEVEL="1"
   fi
+  echo "RAID ENABLED"
+  echo "RAID Devices: sda${MY_RAID_SLAVE}"
+  echo "Set RAID level to ${MY_RAID_LEVEL}"
 fi
-
-
-
 
 # check for ram size
 #if [[ $(( $(vmstat -s | grep -i "total memory" | xargs | cut -d" " -f 1) / 1024 / 1000)) -le "64" ]] ; then
 
+#### CONFIGURE SWAP
 # HDD more than 400gb = 64GB swap
 # HDD more than 160gb = 32GB swap
 # HDD less than 160gb = 16GB swap
@@ -103,8 +107,87 @@ else
     exit 1
   fi
 fi
+echo "Set SWAP size to ${MY_SWAP} GBytes"
 
-echo "Set swap size to ${MY_SWAP} GBytes"
+#### CONFIGURE SLOG PARTITIONS
+if [ "$(cat /sys/block/sda/queue/rotational)" == "1" ] ; then
+  echo "HDD Detected, ignoring slog partition"
+  MY_SLOG=""
+elif [ "$MY_RAID_LEVEL" == "10" ]; then
+  echo "SSD Detected, RAID 10 enabled, ignoring slog partition"
+  MY_SLOG=""
+else
+  if [ "$(cat /sys/block/sdb/queue/rotational)" == "1" ] || [ "$(cat /sys/block/sdc/queue/rotational)" == "1" ]  || [ "$(cat /sys/block/sdd/queue/rotational)" == "1" ] || [ "$(cat /sys/block/sde/queue/rotational)" == "1" ] || [ "$(cat /sys/block/sdf/queue/rotational)" == "1" ] ; then
+    echo "HDD Detected with SSD, enabling slog partition"
+    #### CONFIGURE CACHE
+    # HDD more than 800gb = 120GB CACHE
+    # HDD more than 400gb = 60GB CACHE
+    # HDD more than 160gb = 30GB CACHE
+    # HDD less than 160gb = DISABLE CACHE
+    echo "Detecting and setting optimal slog partition size"
+    if [[ $(awk '/sda$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) -gt "800" ]] ; then
+      MY_SLOG="10"
+    elif [[ $(awk '/sda$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) -gt "400" ]] ; then
+      MY_SLOG="5"
+    elif [[ $(awk '/sda$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) -gt "160" ]] ; then
+      MY_SLOG="1"
+    else
+      MY_SLOG=""
+    fi
+  else
+    if ! [[ $MY_SLOG =~ ^[0-9]+$ ]] ; then
+      echo "error: MY_SLOG is Not a number, specify in GB"
+      exit 1
+    fi
+  fi
+fi
+
+
+#### CONFIGURE CACHE PARTITIONS
+if [ "$(cat /sys/block/sda/queue/rotational)" == "1" ] ; then
+  echo "HDD Detected, ignoring cache partition"
+  MY_CACHE=""
+elif [ "$MY_RAID_LEVEL" == "10" ]; then
+  echo "SSD Detected, RAID 10 enabled, ignoring cache partition"
+  MY_CACHE=""
+else
+  if [ "$(cat /sys/block/sdb/queue/rotational)" == "1" ] || [ "$(cat /sys/block/sdc/queue/rotational)" == "1" ]  || [ "$(cat /sys/block/sdd/queue/rotational)" == "1" ] || [ "$(cat /sys/block/sde/queue/rotational)" == "1" ] || [ "$(cat /sys/block/sdf/queue/rotational)" == "1" ] ; then
+    echo "HDD Detected with SSD, enabling cache partition"
+    #### CONFIGURE CACHE
+    # HDD more than 800gb = 120GB CACHE
+    # HDD more than 400gb = 60GB CACHE
+    # HDD more than 160gb = 30GB CACHE
+    # HDD less than 160gb = DISABLE CACHE
+    echo "Detecting and setting optimal swap partition size"
+    if [[ $(awk '/sda$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) -gt "800" ]] ; then
+      MY_CACHE="120"
+    elif [[ $(awk '/sda$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) -gt "400" ]] ; then
+      MY_CACHE="60"
+    elif [[ $(awk '/sda$/{printf "%i", $(NF-1) / 1000 / 1000}' /proc/partitions) -gt "160" ]] ; then
+      MY_CACHE="30"
+    else
+      MY_CACHE=""
+    fi
+  else
+    if ! [[ $MY_CACHE =~ ^[0-9]+$ ]] ; then
+      echo "error: ${MY_CACHE} is Not a number, specify in GB"
+      exit 1
+    fi
+  fi
+fi
+
+
+if [ "$MY_CACHE" != "" ]; then
+  echo "CACHE: ${MY_CACHE}"
+  MY_CACHE=",/xshok/zfs-cache:ext4:${MY_CACHE}G"
+fi
+if [ "$MY_SLOG" != "" ]; then
+  echo "SLOG: ${MY_SLOG}"
+  MY_SLOG=",/xshok/zfs-slog:ext4:${MY_SLOG}G"
+fi
+
+#wait 5 seconds
+sleep 5
 
 #fetching post install
 curl "https://raw.githubusercontent.com/hetzneronline/installimage/master/post-install/proxmox5" --output /post-install
