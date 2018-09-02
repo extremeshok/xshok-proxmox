477c2af8c47aaab1fe8e790e0e03d6204fc9655e
eXtremeSHOK <admin@extremeshok.com>
2018-09-02 06:50:43 +0200

    fix missing command

--- benchmark_zfs.sh
+++ benchmark_zfs.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # https://blog.programster.org/zfs-add-intent-log-device
 
+if [ ! -e /usr/bin/time ] ; then
+  apt-get install time
+fi
+
 echo "Performing cached write of 100,000 4k blocks..."
 /usr/bin/time -f "%e" sh -c 'dd if=/dev/zero of=4k-test.img bs=4k count=100000 2> /dev/null'
 
