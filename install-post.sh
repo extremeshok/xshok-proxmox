c95d8fb2ff34567e03baa030b1623fdb1861d732
broeckca <48047503+broeckca@users.noreply.github.com>
2020-05-18 16:29:34 +0200

    Update install-post.sh

--- install-post.sh
+++ install-post.sh
@@ -47,7 +47,7 @@ sed -i "s/main contrib/main non-free contrib/g" /etc/apt/sources.list
 apt-get update > /dev/null
 
 ## Remove conflicting utilities
-/usr/bin/env DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' purge ntp openntpd chrony ksm-control-daemon
+/usr/bin/env DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' purge ntp openntpd chrony #ksm-control-daemon
 
 ## Fix no public key error for debian repo
 /usr/bin/env DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' install debian-archive-keyring
