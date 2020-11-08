4f456c94981adbef9bf20f54a3bb5bea4e0dc2dd
88plug <19512127+88plug@users.noreply.github.com>
2020-11-07 20:16:14 -0800

    Update install-post.sh

--- install-post.sh
+++ install-post.sh
@@ -107,6 +107,9 @@ echo "Y" | pveceph install
 ## Install Docker
 /usr/bin/env DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' install docker.io docker-compose 
 
+## Install kernel source headers for nvidia
+/usr/bin/env DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' install pve-headers 
+
 ## Detect AMD EPYC CPU and install kernel 4.15
 if [ "$(grep -i -m 1 "model name" /proc/cpuinfo | grep -i "EPYC")" != "" ]; then
   echo "AMD EPYC detected"
