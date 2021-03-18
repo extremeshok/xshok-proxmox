e5278d3ee799b53bea4e60448de370d51cb10f53
eXtremeSHOK <admin@extremeshok.com>
2021-03-18 22:26:04 +0200

    nvidia-docker support script

---
+++ nvidia/nvidia-docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# thanks @88plug
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
+  apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+  tee /etc/apt/sources.list.d/nvidia-docker.list
+apt-get update
+
+# Install nvidia-docker2 and reload the Docker daemon configuration
+apt-get install -y nvidia-docker2
+pkill -SIGHUP dockerd
+
+reboot
