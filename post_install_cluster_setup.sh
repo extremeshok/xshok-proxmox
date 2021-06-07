5e7ca3b50a95a88f0813c87dcb325a555a2e0a1b
CasCas2 <caspar@mmail.dk>
2021-06-07 20:05:57 +0200

    Getting info about nag screen

---
+++ post_install_cluster_setup.sh
@@ -0,0 +1,10 @@
+sed -i.backup -z "s/res === null || res === undefined || \!res || res\n\t\t\t.data.status.toLowerCase() \!== 'active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js && systemctl restart pveproxy.service
+
+
+cd /usr/share/javascript/proxmox-widget-toolkit
+cp proxmoxlib.js proxmoxlib.js.backup
+nano proxmoxlib.js
+
+
+Måske denne:
+https://github.com/foundObjects/pve-nag-buster
